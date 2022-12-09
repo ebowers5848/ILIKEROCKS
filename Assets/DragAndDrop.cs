5e256e924d4ae80cf03931270dcc6494b9ee8770
Ezra Serebrin <ezraserebrin@gmail.com>
2022-12-09 16:48:40 -0600

    body rotation drag and drop

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -7,6 +7,10 @@ public class DragAndDrop : MonoBehaviour {
     Collider2D collider;
     public GameObject effector;
 
+	// for use in RotateBody()
+    public GameObject body;
+    public GameObject head;
+
     void Start() {
         collider = GetComponent<Collider2D>();
         dragging = false;
@@ -23,11 +27,20 @@ public class DragAndDrop : MonoBehaviour {
 
         if (dragging) {
             this.transform.position = mousePos;
+
+            if (gameObject.tag == "body_target") {
+                RotateBody();
+            }
         }
 
         if (Input.GetMouseButtonUp(0)) {
             this.transform.position = effector.transform.position;
             dragging = false;
         }
+    }
+
+    void RotateBody() {
+        float angle = Vector3.Angle(body.transform.up, head.transform.position - this.transform.position);
+        body.transform.rotation = Quaternion.RotateTowards(body.transform.rotation, Quaternion.Euler(0, 0, angle), 1);
     }
 }
