9c768626df8435d678c7ec2aaa2efafd270ca5c9
Ezra Serebrin <ezraserebrin@gmail.com>
2022-12-13 12:20:54 -0600

    scenes and buttons

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -5,4 +5,12 @@ public class LevelSelector : MonoBehaviour {
     public void Select(string levelName) {
         SceneManager.LoadScene(levelName);
     }
+
+    public void Credits() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Help() {
+        SceneManager.LoadScene("Help");
+    }
 }
